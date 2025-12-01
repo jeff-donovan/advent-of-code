74d22f75456b7098afd1c9b8616793cc2502987d
Jeff Donovan <jdonovan@mariners.com>
2025-12-01 00:10:51 -0800

    rename makeCodes -> parseInput, use test

--- 2025/1/1.1.go
+++ 2025/1/1.1.go
@@ -7,36 +7,37 @@ import (
 	"time"
 )
 
-func makeCodes(f *os.File) ([]string, error) {
-	var codes []string
+func parseInput(f *os.File) ([]string, error) {
+	var lines []string
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		codes = append(codes, scanner.Text())
+		lines = append(lines, scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
 		return nil, err
 	}
 
-	return codes, nil
+	return lines, nil
 }
 
 func main() {
-	f, err := os.Open("C:/code/advent-of-code/2025/1/day_1_input.txt")
+	// f, err := os.Open("C:/code/advent-of-code/2025/1/day_1_input.txt")
+	f, err := os.Open("C:/code/advent-of-code/2025/1/day_1_test.txt")
 	if err != nil {
 		fmt.Println("Error opening file", err)
 		return
 	}
 
 	defer f.Close()
-	codes, err := makeCodes(f)
+	lines, err := parseInput(f)
 	if err != nil {
 		fmt.Println("Error reading file:", err)
 	}
 
 	start := time.Now()
 
-	for _, code := range codes {
+	for _, code := range lines {
 		fmt.Println(code)
 	}
 
