838c62824e68f3cc59af626bcffdae67b9d3919d
Jeff Donovan <jdonovan@mariners.com>
2025-12-04 20:47:04 -0800

    set up day 5 boilerplate

---
+++ 2025/5/5.1.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
+
+func parseInput(f *os.File) ([]string, error) {
+	var lines []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
+}
+
+func main() {
+	// f, err := os.Open("C:/code/advent-of-code/2025/5/day_5_input.txt")
+	f, err := os.Open("C:/code/advent-of-code/2025/5/day_5_test.txt")
+	if err != nil {
+		fmt.Println("Error opening file", err)
+		return
+	}
+
+	defer f.Close()
+	lines, err := parseInput(f)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+	start := time.Now()
+
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+
+	// fmt.Println("Answer: ", total)
+
+	fmt.Println("took: ", time.Since(start))
+}
