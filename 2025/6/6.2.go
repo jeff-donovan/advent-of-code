0e6606d27cffcd030a1d5267413a8db5a303f8e3
Jeff Donovan <jdonovan@mariners.com>
2025-12-06 18:23:56 -0800

    solve 6.2

--- 2025/6/6.2.go
+++ 2025/6/6.2.go
@@ -1,8 +1,53 @@
 package main
 
+import (
+	"strconv"
+)
+
+func mapInputToMathProblemsPart2(lines []string) []MathProblem {
+	var problems []MathProblem
+	operationIndex := len(lines) - 1
+
+	var nums []int
+	for n := len(lines[0]) - 1; n >= 0; n-- {
+		numString := ""
+		for j, l := range lines {
+			cleanedChar := string(l[n])
+			if cleanedChar == " " {
+				cleanedChar = ""
+			}
+
+			if j != operationIndex {
+				numString += cleanedChar
+				continue
+			}
+
+			// we're now at the operation line so let's turn the string into an int
+			// but only if it's not an empty string, otherwise that gets cast as 0
+			if numString != "" {
+				num, _ := strconv.Atoi(numString)
+				nums = append(nums, num)
+				numString = ""
+			}
+
+			// base case - we've found an operation!
+			if cleanedChar != "" {
+				var pNums []int
+				pNums = append(pNums, nums...)
+				problems = append(problems, MathProblem{cleanedChar, pNums})
+				nums = nil
+				break
+			}
+		}
+	}
+
+	return problems
+}
+
 func algorithm6_2(lines []string) int {
 	total := 0
-	for _, p := range mapInputToMathProblems(cleanLines(lines)) {
+
+	for _, p := range mapInputToMathProblemsPart2(lines) {
 		if p.operation == "*" {
 			total += solveMultiplication(p)
 		}
