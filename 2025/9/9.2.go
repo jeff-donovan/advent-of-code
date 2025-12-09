44f875a981ffe7885964f1fd6450b63ffb7d9c2c
Jeff Donovan <jdonovan@mariners.com>
2025-12-09 00:40:46 -0500

    set up algorithm

--- 2025/9/9.2.go
+++ 2025/9/9.2.go
@@ -2,29 +2,112 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 )
 
+func calculateMinX(coords []Coord) int {
+	min := math.MaxInt
+	for _, c := range coords {
+		if c.x < min {
+			min = c.x
+		}
+	}
+	return min
+}
+
+func calculatemaxY(coords []Coord) int {
+	max := 0
+	for _, c := range coords {
+		if c.y > max {
+			max = c.y
+		}
+	}
+	return max
+}
+
+func getAllAdjacentCoords(a, b Coord) []Coord {
+	if a.x != b.x && a.y != b.y {
+		panic("oops! assumption wrong, these are not matching")
+	}
+
+	if a.x == b.x {
+		minY := a.y
+		if b.y < minY {
+			minY = b.y
+		}
+
+		maxY := a.y
+		if b.y > maxY {
+			maxY = b.y
+		}
+
+		var coords []Coord
+		for y := minY; y <= maxY; y++ {
+			coords = append(coords, Coord{a.x, y})
+		}
+		return coords
+	}
+
+	// otherwise a.y == b.y
+	minX := a.x
+	if b.x < minX {
+		minX = b.x
+	}
+
+	maxX := a.x
+	if b.x > maxX {
+		maxX = b.x
+	}
+
+	var coords []Coord
+	for x := minX; x <= maxX; x++ {
+		coords = append(coords, Coord{x, a.y})
+	}
+	return coords
+}
+
+func makeAllCoordsMap(coords []Coord) map[Coord]struct{} {
+	coordsMap := make(map[Coord]struct{})
+	for i := 0; i < len(coords)-1; i++ {
+		a := coords[i]
+		b := coords[i+1]
+		adjCoords := getAllAdjacentCoords(a, b)
+		for _, c := range adjCoords {
+			coordsMap[c] = struct{}{}
+		}
+	}
+	return coordsMap
+}
+
+func isValidRectangle(coordsMap map[Coord]struct{}, a, b Coord) bool {
+	return false
+}
+
 func algorithm9_2(lines []string) int {
+	// plan
+	//  - get all coords
+	//  - make the coords map
+	//  - figure out how to define a "valid" rectangle
+	//  - loop through and only add the valid rectangle areas
+
 	var coords []Coord
 	for _, l := range lines {
 		numStrings := strings.Split(l, ",")
 		x, _ := strconv.Atoi(numStrings[0])
 		y, _ := strconv.Atoi(numStrings[1])
-		fmt.Println("x: ", x)
-		fmt.Println("y: ", y)
 		coords = append(coords, Coord{x, y})
 	}
 
-	for _, c := range coords {
-		fmt.Println(c)
-	}
+	coordsMap := makeAllCoordsMap(coords)
 
 	var areas []int
 	for _, c1 := range coords {
 		for _, c2 := range coords {
-			areas = append(areas, calculateArea(c1, c2))
+			if isValidRectangle(coordsMap, c1, c2) {
+				areas = append(areas, calculateArea(c1, c2))
+			}
 		}
 	}
 
