e77ed177cf91d6b68bc0f792b408ee3374323828
Jeff Donovan <jdonovan@mariners.com>
2025-12-09 20:55:12 -0800

    add 10 boilerplate

---
+++ 2025/10/10.1.go
@@ -0,0 +1,6 @@
+package main
+
+func algorithm10_1(lines []string) int {
+	total := 0
+	return total
+}
---
+++ 2025/10/10.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	// f, err := os.Open("C:/code/advent-of-code/2025/10/day_10_input.txt")
+	f, err := os.Open("C:/code/advent-of-code/2025/10/day_10_test.txt")
+	if err != nil {
+		fmt.Println("Error opening file", err)
+		return
+	}
+
+	defer f.Close()
+	lines, err := parseInput(f)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+	start := time.Now()
+
+	total := algorithm10_1(lines)
+
+	fmt.Println("Answer: ", total)
+
+	fmt.Println("took: ", time.Since(start))
+}
