2b0470768fad4d7fa422b67b92535a9c9652fed0
Jeff Donovan <jdonovan@mariners.com>
2025-12-10 21:17:11 -0800

    solve part 1!

--- 2025/11/11.go
+++ 2025/11/11.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	// f, err := os.Open("C:/code/advent-of-code/2025/11/day_11_input.txt")
-	f, err := os.Open("C:/code/advent-of-code/2025/11/day_11_test.txt")
+	f, err := os.Open("C:/code/advent-of-code/2025/11/day_11_input.txt")
+	// f, err := os.Open("C:/code/advent-of-code/2025/11/day_11_test.txt")
 	if err != nil {
 		fmt.Println("Error opening file", err)
 		return
