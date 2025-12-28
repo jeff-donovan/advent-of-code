326b28925fac517b3d8f1df02a5a40ffd27c6197
Jeff Donovan <jdonovan@mariners.com>
2025-12-27 23:24:29 -0800

    add helper for backwards map

--- 2025/11/11.2.2.go
+++ 2025/11/11.2.2.go
@@ -1,8 +1,41 @@
 package main
 
+import "fmt"
+
+func makeBackwardsDeviceMap(deviceMap map[string][]string) map[string][]string {
+	backwardsMap := make(map[string][]string)
+
+	for start, endNodes := range deviceMap {
+		for _, end := range endNodes {
+			if value, ok := backwardsMap[end]; ok {
+				backwardsMap[end] = addIfNotExists(value, start)
+			} else {
+				backwardsMap[end] = []string{start}
+			}
+		}
+	}
+
+	return backwardsMap
+}
+
+func addIfNotExists(nodes []string, newNode string) []string {
+	for _, n := range nodes {
+		if n == newNode {
+			return nodes
+		}
+	}
+
+	return append(nodes, newNode)
+}
+
 func algorithm11_2_2(lines []string) int {
 	total := 0
-	makeDeviceMap(lines)
+	deviceMap := makeDeviceMap(lines)
+	backwardsMap := makeBackwardsDeviceMap(deviceMap)
+
+	fmt.Println(deviceMap)
+	fmt.Println()
+	fmt.Println(backwardsMap)
 
 	return total
 }
