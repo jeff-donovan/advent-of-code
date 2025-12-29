36020a347bae200bf2a3a3ffce5322c803f0cc75
Jeff Donovan <jdonovan@mariners.com>
2025-12-29 00:51:25 -0800

    clean up 11.2.2.go

--- 2025/11/11.2.2.go
+++ 2025/11/11.2.2.go
@@ -1,33 +1,5 @@
 package main
 
-import "fmt"
-
-func makeBackwardsDeviceMap(deviceMap map[string][]string) map[string][]string {
-	backwardsMap := make(map[string][]string)
-
-	for start, endNodes := range deviceMap {
-		for _, end := range endNodes {
-			if value, ok := backwardsMap[end]; ok {
-				backwardsMap[end] = addIfNotExists(value, start)
-			} else {
-				backwardsMap[end] = []string{start}
-			}
-		}
-	}
-
-	return backwardsMap
-}
-
-func addIfNotExists(nodes []string, newNode string) []string {
-	for _, n := range nodes {
-		if n == newNode {
-			return nodes
-		}
-	}
-
-	return append(nodes, newNode)
-}
-
 func dfsCountPaths(graph map[string][]string) int {
 	numPathsFromSvrToFft := dfs(graph, make(map[string]int), "svr", "fft")
 	numPathsFromFftToDac := dfs(graph, make(map[string]int), "fft", "dac")
@@ -57,18 +29,7 @@ func dfs(graph map[string][]string, paths map[string]int, currentNode string, en
 }
 
 func algorithm11_2_2(lines []string) int {
-	// total := 0
 	deviceMap := makeDeviceMap(lines)
 
-	// fmt.Println(deviceMap)
-	// fmt.Println()
-
-	fmt.Println("deviceMap: ", len(deviceMap))
-
 	return dfsCountPaths(deviceMap)
-	// fmt.Println()
-	// fmt.Println("nodes to consider: ", len(nodesToConsider))
-	// // fmt.Println(nodesToConsider)
-
-	// return total
 }
